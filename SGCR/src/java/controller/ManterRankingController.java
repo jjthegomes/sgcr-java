6c23a46ef823c84a2eeacdbd5864bb934602260b
arielsonalt <32400603+arielsonalt@users.noreply.github.com>
2017-10-30 15:17:03 -0200

    ManterRankingController

---
+++ SGCR/src/java/controller/ManterRankingController.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import modelo.Administrador;
+
+/**
+ *
+ * @author RAJ
+ */
+public class ManterRankingController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String acao= request.getParameter("acao");
+        if(acao.equals("prepararIncluir")){
+            prepararIncluir(request, response);
+        }
+    }
+    
+    public void prepararIncluir(HttpServletRequest request, HttpServletResponse response){
+        try{
+            request.setAttribute("operacao", "Incluir");
+            request.setAttribute("administradores", Administrador.obterAdministradores());
+            RequestDispatcher view = request.getRequestDispatcher("/manterRanking.jsp");
+            view.forward(request, response);
+            
+        }catch(ServletException ex){
+        }catch(IOException ex){
+        }catch (ClassNotFoundException ex){
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
